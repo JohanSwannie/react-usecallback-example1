22cf39aa26578638af840818b6adfba08ddafa92
jswan <jswannieman@gmail.com>
2023-07-27 17:23:22 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { useState } from "react";
+import ListNumbers from "./ListNumbers";
 
 function App() {
-  return <div className="App"></div>;
+  const [number, setNumber] = useState(1);
+  const [blueTheme, setBlueTheme] = useState(false);
+
+  const getNumbers = (passedNumber) => {
+    return [
+      number + passedNumber,
+      (number + 1 + passedNumber) / 2,
+      (number + 2 + passedNumber) / 3,
+      (number + 3 + passedNumber) / 4,
+      (number + 4 + passedNumber) / 5,
+    ];
+  };
+
+  const appTheme = {
+    color: blueTheme ? "#FFF" : "#000",
+    backgroundColor: blueTheme ? "navy" : "#FFF",
+  };
+
+  return (
+    <div className="App" style={appTheme}>
+      <input
+        type="number"
+        value={number}
+        onChange={(event) => setNumber(parseInt(event.target.value))}
+      />
+      <button onClick={() => setBlueTheme((prevBlueTheme) => !prevBlueTheme)}>
+        Toggle between normal and Blue Theme
+      </button>
+      <ListNumbers getNumbers={getNumbers} />
+    </div>
+  );
 }
 
 export default App;
