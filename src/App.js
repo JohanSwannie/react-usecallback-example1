d6b80cbcbf4ea7859dcd999f1defee739a083a7e
jswan <jswannieman@gmail.com>
2023-07-28 11:56:19 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import ListNumbers from "./ListNumbers";
 
 function App() {
   const [number, setNumber] = useState(1);
   const [blueTheme, setBlueTheme] = useState(false);
 
-  const getNumbers = (passedNumber) => {
-    return [
-      number + passedNumber,
-      (number + 1 + passedNumber) / 2,
-      (number + 2 + passedNumber) / 3,
-      (number + 3 + passedNumber) / 4,
-      (number + 4 + passedNumber) / 5,
-    ];
-  };
+  const getNumbers = useCallback(
+    (passedNumber) => {
+      return [
+        number + passedNumber,
+        (number + 1 + passedNumber) / 2,
+        (number + 2 + passedNumber) / 3,
+        (number + 3 + passedNumber) / 4,
+        (number + 4 + passedNumber) / 5,
+      ];
+    },
+    [number]
+  );
 
+  const handleChangedNumber = (event) => {
+    if (event.target.value > 0) {
+      setNumber(parseInt(event.target.value));
+    } else {
+      setNumber(1);
+    }
+  };
   const appTheme = {
     color: blueTheme ? "#FFF" : "#000",
     backgroundColor: blueTheme ? "navy" : "#FFF",
@@ -22,11 +32,7 @@ function App() {
 
   return (
     <div className="App" style={appTheme}>
-      <input
-        type="number"
-        value={number}
-        onChange={(event) => setNumber(parseInt(event.target.value))}
-      />
+      <input type="number" value={number} onChange={handleChangedNumber} />
       <button onClick={() => setBlueTheme((prevBlueTheme) => !prevBlueTheme)}>
         Toggle between normal and Blue Theme
       </button>
